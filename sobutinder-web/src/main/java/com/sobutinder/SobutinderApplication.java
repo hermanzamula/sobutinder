af3928184a27ed304c625a4fa1d5331e4bb4d6ba
Herman Zamula <herman.zamula@gmail.com>
2016-08-14 17:59:58 +0300

    Added FB oauth Migrate config to yaml

--- sobutinder-web/src/main/java/com/sobutinder/SobutinderApplication.java
+++ sobutinder-web/src/main/java/com/sobutinder/SobutinderApplication.java
@@ -2,11 +2,13 @@ package com.sobutinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class SobutinderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SobutinderApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SobutinderApplication.class, args);
+    }
 }
