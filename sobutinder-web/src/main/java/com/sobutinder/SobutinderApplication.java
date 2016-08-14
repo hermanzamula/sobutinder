3a7ffacfeb6366305111865cef2db307a8f3c0f4
Herman Zamula <herman.zamula@gmail.com>
2016-08-14 16:32:47 +0300

    Add spring webapp

---
+++ sobutinder-web/src/main/java/com/sobutinder/SobutinderApplication.java
@@ -0,0 +1,12 @@
+package com.sobutinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SobutinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SobutinderApplication.class, args);
+	}
+}
